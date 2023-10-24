c75cad76f8fa9bd18918e6be9f50a6e096351aac
John Bright <jmbright400@gmail.com>
2023-10-24 15:23:27 -0500

    adjustments made to previous code.

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            string reversed = new string(word.Reverse().ToArray());
+            return word == reversed;
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("madam", true)]
+        [InlineData("noon", true)]
+        [InlineData("level", true)]
+        [InlineData("rotor", true)]
+        [InlineData("sunglasses", false)]
+        [InlineData("at", false)]
+        [InlineData("night", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
-
+            var wordsmith = new WordSmith();
+            var actual = wordsmith.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
